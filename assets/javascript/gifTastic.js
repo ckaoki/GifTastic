dd2a2afe8b62042ff02a6b286aa964917718cf62
ckaoki <cullan_aoki@msn.com>
2019-06-10 22:30:40 -0700

    able to display images when initial buttons pressed but dynamically added buttons don't work even though their tags are exactly the same. hmmm...

--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -3,39 +3,61 @@ var animals = ["dog", "cat", "horse", "frog"];
 // populate default buttons
 
 
-function addButton(animal){
-  var button = $("<button/>");
-  console.log(animal);
-  button.text(animal);
-  button.val(animal);
-  button.addClass("btn btn-primary");
-  button.css("margin-right", "10px");
-  button.css("margin-bottom", "10px");
-  $("#buttonsDiv").append(button);
-}
+function addButtons(){
+  $("#buttonsDiv").empty();
+  for(var i=0; i<animals.length; i++){
+    var button = $("<button/>");
+    console.log(animals[i]);
+    button.text(animals[i]);
+    button.val(animals[i]);
+    button.addClass("btn btn-primary animalButton");
+    button.css("margin-right", "10px");
+    button.css("margin-bottom", "10px");
+    $("#buttonsDiv").append(button);
+  }
+};
 
 $( document ).ready(function() {
   console.log( "ready!" );
   // Adding default button to the HTML
-  for(var i=0; i<animals.length; i++){
-    addButton(animals[i]);    
-  };
+     addButtons();    
+
   
-  $("#submit").on("click",function(event){
-    event.preventDefault();
-    addButton($("#newAnimalName").val());
-    console.log($("#newAnimalName").val());
+  $("#submit").on("click",function(){
+    animals.push($("#newAnimalName").val());
+    addButtons();
   });
   
-  // Add this to the button click event    
-  // var queryURL="https://api.giphy.com/v1/gifs/search?q="+ animals[i] + "&api_key=7lp1mk5d6dEajvw77jQLtFsuo8PB0kRn&limit=1";
-  // console.log(queryURL);
+  // Animal button click event
+  $(".animalButton").on("click", function(event){  
+    event.preventDefault();
+    
+    var queryURL="https://api.giphy.com/v1/gifs/search?q="+ $(this).text() + "&api_key=7lp1mk5d6dEajvw77jQLtFsuo8PB0kRn&limit=10";
+    console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .then(function(response){
+      result = response.data;
+      for(i=0; i<result.length; i++){
+        //create new image tag for each image
+        console.log(i);
+        var newImage = $("<img>");
+        newImage.addClass("animalImage");
+        newImage.attr("data-still",result[i].images.fixed_height_still.url);
+        newImage.attr("data-animated",result[i].images.fixed_height.url);
+        newImage.attr("src", result[i].images.fixed_height_still.url);
+        newImage.css("margin: 20px");
+        $("#gifsDiv").append(newImage);
+      }
+    });
+  }); 
   // $.ajax({
         //     url: queryURL,
         //     method: "GET"
         // })
         // .then(function(response){
-        //     console.log(i);
         //     results = response.data;
         //     button.attr("data-still", results[0].images.fixed_height_still.url);
         //     });
