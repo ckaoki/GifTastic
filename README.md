9bbfb3bb2819d27dfe27925a299c2541c36da5ec
ckaoki <cullan_aoki@msn.com>
2019-06-11 17:42:52 -0700

    fully functional Giphy API queries. Buttons work properly and display still images. Image animate/still when clicked on.  Still need to updated portfolio and ad link to readme.

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # GifTastic
+This project contains a web page which pulls images using the GIPHY API and displays them to the user at the click of a button. The user may also create custom buttons to search GIPHY. This page was designed using Bootstrap and is responsive.
+
+Overview
+1. This web page contains a default set of search buttons that are created dynamically on document load.
+2. When the user clicks on a button, the page grabs 10 static, non-animated gif images from the GIPHY API and places them on the page.
+3. Under each image its rating is displayed.
+4. When the user clicks one of the still GIPHY images, the gif animates. If the user clicks the gif again, it stops playing. 
+5. A form is used to take a value from a user input box and adds a new button to the topics array. 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+
+### Prerequisites
+
+Browser minimum requirements:
+* Chrome ver 74
+* Firefox ver 60
+
+### Installing
+
+To view files navigate to  https://ckaoki.github.io/GifTastic/.
+
+To install source files use git to clone files from https://github.com/ckaoki/GifTastic.
+
+The app.js file contains the trivia game code.
+
+## Running the tests
+
+Open files in browser and make sure they are properly formatted. Your eyes are the test.
+
+### Break down into end to end tests
+
+Source files have been extensively tested by displaying pages in Google Chrome and using visual verification.
+
+### And coding style tests
+
+I'm starting to get some style.
+
+## Deployment
+
+Navigate to https://github.com/ckaoki/GifTastic to clone.
+
+## Built With
+
+* [Visual Studio Code](https://code.visualstudio.com/)
+
+## Contributing
+
+Please read our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use github for version control (https://github.com/your/project/tags). 
+
+## In development
+Implementing textual hints.
+
+## Authors
+
+* **Cullan Aoki** - *Initial work* - https://github.com/ckaoki/GifTastic
+
+## License
+
+This project is not licensed.
+
+## Acknowledgments
+
+* Joe Rehfuss
+* Trae Shanks
+* Lan Truong
+* James Hanley
--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -1,39 +1,41 @@
-var animals = ["dog", "cat", "horse", "frog"]; 
+var animals = ["bigfoot", "bird", "cat", "dog", "frog", "horse", "shark", "snake", "spider", "whale"]; 
 
 // populate default buttons
 
 
-function addButtons(){
-  $("#buttonsDiv").empty();
-  for(var i=0; i<animals.length; i++){
-    var button = $("<button/>");
-    console.log(animals[i]);
-    button.text(animals[i]);
-    button.val(animals[i]);
+function addButtons(animal){
+  // $("#buttonsDiv").empty();
+
+    var button = $("<button>");
+    console.log(animal);
+    button.text(animal);
+    button.val(animals);
     button.addClass("btn btn-primary animalButton");
     button.css("margin-right", "10px");
     button.css("margin-bottom", "10px");
     $("#buttonsDiv").append(button);
-  }
-};
+  };
 
-$( document ).ready(function() {
-  console.log( "ready!" );
-  // Adding default button to the HTML
-     addButtons();    
+
+  $( document ).ready(function() {
+    console.log( "ready!" );
+    // Adding default button to the HTML
+    for(var i=0; i<animals.length; i++){
+    addButtons(animals[i]);  
+  } ; 
 
   
-  $("#submit").on("click",function(){
-    animals.push($("#newAnimalName").val());
-    addButtons();
+  $("#submit").on("click",function(event){
+    event.preventDefault();
+    // animals.push($("#newAnimalName").val());
+    addButtons($("#newAnimalName").val());
   });
   
-  // Animal button click event
-  $(".animalButton").on("click", function(event){  
-    event.preventDefault();
-    
-    var queryURL="https://api.giphy.com/v1/gifs/search?q="+ $(this).text() + "&api_key=7lp1mk5d6dEajvw77jQLtFsuo8PB0kRn&limit=10";
+  // display images when animal button clicked
+  function displayImages(){
+    var queryURL="https://api.giphy.com/v1/gifs/search?q="+ $(this).text() + "&api_key=7lp1mk5d6dEajvw77jQLtFsuo8PB0kRn&rating=pg-13&limit=10";
     console.log(queryURL);
+    $("#gifsDiv").empty();
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -41,24 +43,42 @@ $( document ).ready(function() {
     .then(function(response){
       result = response.data;
       for(i=0; i<result.length; i++){
-        //create new image tag for each image
-        console.log(i);
+        //create new elements for each image
+        var newDiv = $("<div>");
+        var newP = $("<p>");
         var newImage = $("<img>");
         newImage.addClass("animalImage");
-        newImage.attr("data-still",result[i].images.fixed_height_still.url);
-        newImage.attr("data-animated",result[i].images.fixed_height.url);
         newImage.attr("src", result[i].images.fixed_height_still.url);
-        newImage.css("margin: 20px");
-        $("#gifsDiv").append(newImage);
+        newImage.attr("data-still", result[i].images.fixed_height_still.url);
+        newImage.attr("data-animated", result[i].images.fixed_height.url);
+        newImage.attr("state", "still");
+        newP.text("Rating: " + result[i].rating);
+        newP.addClass("rating");
+        newDiv.append(newImage);
+        newDiv.append(newP);
+        $("#gifsDiv").append(newDiv);
       }
     });
-  }); 
-  // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // })
-        // .then(function(response){
-        //     results = response.data;
-        //     button.attr("data-still", results[0].images.fixed_height_still.url);
-        //     });
-});
+  }; 
+
+  // toggle between still and animated images when clicked on
+  function toggleImage(){
+    if($(this).attr("state") === "still"){
+      $(this).attr("src", $(this).attr("data-animated"));
+      $(this).attr("state", "animated");
+    }
+    else{
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("state", "still");
+    }
+
+     
+  };
+
+
+  // Event listener for dynamically generated elements
+  $(document).on("click", ".animalButton", displayImages);
+  $(document).on("click", ".animalImage", toggleImage);
+
+
+  });
